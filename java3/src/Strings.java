0bc61e0531cd04c6a097d77be662a4c78d7c30bb
abobus <zhdanp95@mail.ru>
2022-06-17 15:48:07 +0300

    fix:strings1

---
+++ java3/src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+public static void main(String[] args ){
+    int x = 5;
+    String s = "hello";
+    String space = " ";
+    String name = "Debik";
+    System.out.println(s+space+name);
+    System.out.println(x+name);
+    System.out.println("hello"+" "+name);
+}
+}
